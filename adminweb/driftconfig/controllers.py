b420bfc1de058ce66c39ea7c058898649e204e81
Jon Bjarnason <nonnib@gmail.com>
2018-10-07 12:50:34 +0000

    Added basic org and product view pages to config and cleaned up layout.

--- adminweb/driftconfig/controllers.py
+++ adminweb/driftconfig/controllers.py
@@ -7,17 +7,64 @@ import logging
 log = logging.getLogger(__name__)
 bp = Blueprint('driftconfig', __name__, url_prefix='/driftconfig', template_folder="driftconfig")
 
+ts, source = get_default_drift_config_and_source()
+
 
 @bp.route('/')
 @login_required
 def index():
-    ts, source = get_default_drift_config_and_source()
+    return render_template('driftconfig/index.html')
+
+
+@bp.route('/organizations')
+@login_required
+def organizations():
+    return render_template('driftconfig/organizations.html',
+                           organizations=ts.tables.get('organizations')._rows.values(),
+                           ts=ts)
+
+
+@bp.route('/organizations/<organization_name>')
+@login_required
+def organization(organization_name):
+    tbl = ts.get_table('organizations')
+    organization_row = tbl.find({"organization_name": organization_name})[0]
+    products = ts.get_table('products').find({"organization_name": organization_name})
+
+    return render_template('driftconfig/organization.html',
+                           organization=organization_row,
+                           products=products,
+                           ts=ts)
+
+
+@bp.route('/products')
+@login_required
+def products():
+    return render_template('driftconfig/products.html',
+                           products=ts.tables.get('products')._rows.values(),
+                           ts=ts)
+
+
+@bp.route('/organizations/<organization_name>/products/<product_name>')
+@login_required
+def product(organization_name, product_name):
+    tbl = ts.get_table('products')
+    product_row = tbl.find({"product_name": product_name, "organization_name": organization_name})[0]
+
+    return render_template('driftconfig/product.html',
+                           product=product_row,
+                           ts=ts)
+
+
+@bp.route('/tablestore')
+@login_required
+def tablestore():
     meta_by_table = {}
     for tbl in ts.meta['tables']:
         meta_by_table[tbl['table_name']] = tbl
-    return render_template('driftconfig/index.html',
+    return render_template('driftconfig/tablestore.html',
                            tables=sorted(ts.tables.keys()),
-                           ts=ts, 
+                           ts=ts,
                            source=source,
                            meta=ts.meta,
                            meta_by_table=meta_by_table)
